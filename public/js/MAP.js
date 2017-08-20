f30ef7f24ee8b8b8e65c6e482b34977486c790d7
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2017-08-20 01:52:06 -0400

    js stuff

---
+++ public/js/MAP.js
@@ -0,0 +1,14 @@
+globals = {
+  lookdownAngle: -50
+};
+
+AFRAME.registerComponent('map-component', { 
+  tick: function() {
+    var rotation = this.el.getAttribute('rotation');
+    if (rotation != null) {
+      if (rotation.x < globals.lookdownAngle) {
+        document.getElementById("map").setAttribute('visible', true);
+      } else {
+        document.getElementById("map").setAttribute('visible', false);
+      }
+  }}});
